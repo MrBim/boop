fdb67614bbbbb52fbcbc768705b1ac338a018cf6
MrBim <bimtech84@gmail.com>
2023-06-04 08:13:04 +0100

    Introducing the game class

--- js/felines.js
+++ js/felines.js
@@ -1,17 +1,41 @@
-class feline {
-  maturity = 'kitten';
+// on game load, create 8 felines ( in kitten state )
+// add them to each players pool of felines
+
+// when a player clicks a playable square, a kitten is placed in that square.
+// any kittens in adjcent squares are booped away from the places kitten (with some caviats)
+
+class Game {
+  playersTurn = 1;
+  felines = [];
+  startGame() {
+    [...Array(16).keys()].forEach((it) => {
+      this.felines.push(new Feline(it))
+    })
+  }
+  nextTurn() {
+    this.playersTurn = Math.abs(playersTurn - 1);
+  }
+}
+
+class Feline {
+  id = 'kitten-1';
+  player;
   x = 0;
   y = 0;
-  player = 0;
+  maturity;
   isInPlay = false;
-  id = 'kitten-1';
+  domNode
 
-  constructor(x, y, maturity, player) {
-    this.x = x;
-    this.y = y;
-    this.maturity = maturity;
-    this.player = player;
+  constructor(player) {
+    this.maturity = 'kitten';
+    this.player = player % 2;
+    this.domNode = document.getElementById(id)
   };
+
+  addToBoard(x, y) {
+    this.setPosition(x,y);
+    this.isInPlay = true; 
+  }
 
   getPosition() {
     return [this.x, this.y];

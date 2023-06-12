4a09f56cdb1ae5f17f84d4db4658c7947522e92d
MrBim <bimtech84@gmail.com>
2023-06-12 21:43:15 +0100

    can place kittys, no booping yet

--- js/felines.js
+++ js/felines.js
@@ -19,20 +19,18 @@ class Game {
     this.gameBoard = document.getElementById('game')
     // break this out in to its own method.
     this.gameBoard.addEventListener('click', (e) => {
-      const newX = Math.floor((e.x - 200)/ kittenWidth)
+      const newX = Math.floor((e.x - 200) / kittenWidth)
       const newY = Math.floor((e.y) / kittenWidth)
 
-      console.log('newX, newY', newX, newY, this.playersTurn)
+      const designatedFeline = this.felines.filter((it) => (it.player === this.playersTurn) && !it.isInPlay)[0];
+      this.placeFeline(designatedFeline, newX, newY)
+      this.boop(newX, newY)
       this.nextTurn()
-      // const newFeline = makeNewFeline(newY, newX
-      // gameBoard.appendChild(newFeline)
-      // boop(newX, newY)
-      // updateTurn()
-  }, false)
-  
-    ;[...Array(16).keys()].forEach((it) => {
-      this.felines.push(new Feline(it, this.playerOneSpace, this.playerTwoSpace))
-    })
+    }, false)
+
+      ;[...Array(16).keys()].forEach((it) => {
+        this.felines.push(new Feline(it, this.playerOneSpace, this.playerTwoSpace))
+      })
 
   }
   nextTurn() {
@@ -41,13 +39,72 @@ class Game {
   getTurn() {
     return this.playersTurn
   }
-  placeFeline(feline) {
+  placeFeline(feline, x, y) {
+    feline.isInPlay = true
+    this.moveFeline(feline, x, y)
+  }
+  moveFeline(feline, x, y) {
+    feline.domNode.style.cssText = `top: ${y * 12.5}%; left: ${x * 12.5}%;`
+    feline.x = x
+    feline.y = y
+    this.gameBoard.appendChild(feline.domNode)
+  }
+  boop = (x, y) => {
+    const kittens = this.felines
+    const kittyPositions = [];
 
+    for (let i = 0; i < kittens.length; i++) {
+      const kitten = kittens[i].node;
+      const top = kittens[i].y
+      const left = kittens[i].x
+      kittyPositions.push([top, left]);
+    }
+
+    for (let i = 0; i < kittens.length; i++) {
+      const kitten = kittens[i].domNode
+      const top = kittens[i].y
+      const left = kittens[i].x
+
+      if (kittyShouldBoop(top, left, x, y)) {
+        let newTop = `${top}%`
+        let newLeft = `${left}%`
+
+        if ((top / 12.5) === (y + 1)) {
+          newTop = `${top + 12.5}%`;
+        }
+        if ((top / 12.5) === (y - 1)) {
+          newTop = `${top - 12.5}%`;
+        }
+        if ((left / 12.5) === (x + 1)) {
+          newLeft = `${left + 12.5}%`;
+        }
+        if ((left / 12.5) === (x - 1)) {
+          newLeft = `${left - 12.5}%`;
+        }
+
+        if (spaceNotOccupied(kittyPositions, [newTop, newLeft])) {
+          kitten.style.top = newTop;
+          kitten.style.left = newLeft;
+          kitten.style.cssText = `top: ${newTop}; left: ${newLeft};`
+        }
+        if (newTop === '0%' || newTop === '87.5%' || newLeft === '0%' || newLeft === '87.5%') {
+          setTimeout(() => {
+            kitten.remove();
+          }, 1250);
+        }
+      }
+    }
   }
 }
+const spaceNotOccupied = (kittyPos, newPos) => !kittyPos.filter(it => `${it[0]}%` === newPos[0] && `${it[1]}%` === newPos[1]).length;
 
+const kittyShouldBoop = (top, left, x, y) => {
+  return (((top / 12.5) === (y + 1)) || ((top / 12.5) === (y - 1)) || ((top / 12.5) === (y)))
+    && ((left / 12.5 === (x + 1) || left / 12.5 === (x - 1)) || ((left / 12.5) === (x)))
+    && !(((top / 12.5) === (y)) && ((left / 12.5) === (x)));
+}
 class Feline {
-  id = 'kitten-1'
+  id = 1
   player
   x = 0
   y = 0
@@ -56,15 +113,17 @@ class Feline {
   domNode
 
   constructor(player, playerOneSpace, playerTwoSpace) {
+    this.id = player
     this.maturity = 'kitten'
     this.player = player % 2
     this.domNode = this.makeNewFeline(player)
+
     if (!(player % 2)) {
       playerOneSpace.appendChild(this.domNode)
     } else {
       playerTwoSpace.appendChild(this.domNode)
     }
-    this.domNode
+
   }
 
   addToBoard(x, y) {
@@ -105,7 +164,6 @@ class Feline {
 const runGame = () => {
   console.log('running game')
   const myGame = new Game()
-  myGame.startGame()
 }
 
 function ready(fn) {

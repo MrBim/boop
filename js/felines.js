2b6a283de27741dc3fc939bdce1b4343b1f92e19
MrBim <bimtech84@gmail.com>
2023-06-13 12:12:51 +0100

    felines boop incorrectly.

--- js/felines.js
+++ js/felines.js
@@ -10,19 +10,22 @@ class Game {
   gameBoard
   playersTurn = 1
   felines = []
+
   constructor() {
     this.startGame()
   }
+
   startGame() {
     this.playerOneSpace = document.getElementById('playerSpace1')
     this.playerTwoSpace = document.getElementById('playerSpace2')
     this.gameBoard = document.getElementById('game')
+
     // break this out in to its own method.
     this.gameBoard.addEventListener('click', (e) => {
       const newX = Math.floor((e.x - 200) / kittenWidth)
       const newY = Math.floor((e.y) / kittenWidth)
-
       const designatedFeline = this.felines.filter((it) => (it.player === this.playersTurn) && !it.isInPlay)[0];
+
       this.placeFeline(designatedFeline, newX, newY)
       this.boop(newX, newY)
       this.nextTurn()
@@ -31,63 +34,71 @@ class Game {
       ;[...Array(16).keys()].forEach((it) => {
         this.felines.push(new Feline(it, this.playerOneSpace, this.playerTwoSpace))
       })
-
   }
+
   nextTurn() {
     this.playersTurn = Math.abs(this.playersTurn - 1)
   }
+
   getTurn() {
     return this.playersTurn
   }
+
   placeFeline(feline, x, y) {
     feline.isInPlay = true
     this.moveFeline(feline, x, y)
   }
+
   moveFeline(feline, x, y) {
     feline.domNode.style.cssText = `top: ${y * 12.5}%; left: ${x * 12.5}%;`
     feline.x = x
     feline.y = y
     this.gameBoard.appendChild(feline.domNode)
   }
-  boop = (x, y) => {
-    const kittens = this.felines
-    const kittyPositions = [];
 
-    for (let i = 0; i < kittens.length; i++) {
-      const kitten = kittens[i].node;
-      const top = kittens[i].y
-      const left = kittens[i].x
-      kittyPositions.push([top, left]);
-    }
+  boop = (x, y) => {
+    console.log('booping', x, y)
+    const kittens = this.felines.filter(it => it.isInPlay)
+
+    kittens.forEach((feline) => {
+      const top = feline.y
+      const left = feline.x
+
+    })
 
     for (let i = 0; i < kittens.length; i++) {
       const kitten = kittens[i].domNode
       const top = kittens[i].y
       const left = kittens[i].x
 
+      console.log('kittens', kittens)
       if (kittyShouldBoop(top, left, x, y)) {
-        let newTop = `${top}%`
-        let newLeft = `${left}%`
+        let newTop = `${top * 12.5}%`
+        let newLeft = `${left * 12.5}%`
+        console.log('kitty should boop', newTop, newLeft)
 
-        if ((top / 12.5) === (y + 1)) {
-          newTop = `${top + 12.5}%`;
+        if ((top) === (y + 1)) {
+          newTop = `${top * 12.5 + 12.5}%`;
         }
-        if ((top / 12.5) === (y - 1)) {
-          newTop = `${top - 12.5}%`;
+        if ((top) === (y - 1)) {
+          newTop = `${top * 12.5 - 12.5}%`;
         }
-        if ((left / 12.5) === (x + 1)) {
-          newLeft = `${left + 12.5}%`;
+        if ((left) === (x + 1)) {
+          newLeft = `${left * 12.5 + 12.5}%`;
         }
-        if ((left / 12.5) === (x - 1)) {
-          newLeft = `${left - 12.5}%`;
+        if ((left) === (x - 1)) {
+          newLeft = `${left * 12.5 - 12.5}%`;
         }
 
-        if (spaceNotOccupied(kittyPositions, [newTop, newLeft])) {
+        if (spaceNotOccupied(kittens, [newTop, newLeft])) {
+          console.log('space not occupied')
           kitten.style.top = newTop;
           kitten.style.left = newLeft;
           kitten.style.cssText = `top: ${newTop}; left: ${newLeft};`
         }
+
         if (newTop === '0%' || newTop === '87.5%' || newLeft === '0%' || newLeft === '87.5%') {
+          console.log('set kitten to be removed')
           setTimeout(() => {
             kitten.remove();
           }, 1250);
@@ -96,13 +107,26 @@ class Game {
     }
   }
 }
-const spaceNotOccupied = (kittyPos, newPos) => !kittyPos.filter(it => `${it[0]}%` === newPos[0] && `${it[1]}%` === newPos[1]).length;
 
-const kittyShouldBoop = (top, left, x, y) => {
-  return (((top / 12.5) === (y + 1)) || ((top / 12.5) === (y - 1)) || ((top / 12.5) === (y)))
-    && ((left / 12.5 === (x + 1) || left / 12.5 === (x - 1)) || ((left / 12.5) === (x)))
-    && !(((top / 12.5) === (y)) && ((left / 12.5) === (x)));
+const spaceNotOccupied = (kittyPos, newPos) => {
+  console.log('kittyPos, newPos', kittyPos, newPos)
+  const val = !kittyPos.filter((it) => {
+    const thing = `${it[0]}%` === newPos[0] && `${it[1]}%` === newPos[1]
+    return thing
+  }).length
+
+  return val
 }
+const kittyShouldBoop = (top, left, x, y) => {
+  // return 
+  console.log('top, left, x, y: ', top, left, x, y)
+  const value = (((top) === (y + 1)) || ((top) === (y - 1)) || ((top) === (y)))
+    && ((left === (x + 1) || left === (x - 1)) || ((left) === (x)))
+    && !(((top) === (y)) && ((left) === (x)))
+  console.log('value', value);
+  return value
+}
+
 class Feline {
   id = 1
   player
@@ -173,6 +197,5 @@ function ready(fn) {
     document.addEventListener('DOMContentLoaded', fn)
   }
 }
-
 
 ready(runGame)
